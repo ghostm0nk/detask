387b6ab9bef28bd9364e60248054e27a742d93ad
Arvin Raed <139760390+ghostm0nk@users.noreply.github.com>
2026-04-16 04:04:01 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { host: '0.0.0.0', allowedHosts: true }
+  server: { host: '0.0.0.0', allowedHosts: true },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react/jsx-dev-runtime', 'react/jsx-runtime']
+  }
 })
